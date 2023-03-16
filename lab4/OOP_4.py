85ec2872b83438e2d2e51943e786c349cfd1f20f
NicoleGeForceRTX <99212272+NicoleGeForceRTX@users.noreply.github.com>
2023-03-16 14:45:00 +0300

    Update OOP_4.py

--- lab4/OOP_4.py
+++ lab4/OOP_4.py
@@ -5,15 +5,22 @@ def accumulate(combiner, null_value, not_filter_value, f, a, next, b, filter):
     elif not filter(a):
         return combiner(accumulate(combiner, null_value, not_filter_value, f, next(a), next, b, filter), not_filter_value)
     else:
-        print(a)
         return combiner(accumulate(combiner, null_value, not_filter_value, f, next(a), next, b, filter), f(a))
 
-f = lambda x: 3 * x * x - 2 * x + 5
+def f(x):
+    return 45 * x * x  + 27 * x * x * x - 8 * x - 7
+
+def summ(a, b):
+    return a + b * dx
+
+def filter(x):
+    return x < -3 or x > 2
+
+def next_value(prev_value):
+    return prev_value + dx
+
 a = -5
-b = 1
+b = 5
 dx = 0.5
 
-combiner = lambda a, b: a + b * dx
-filter = lambda x: x < -4 or x > -3
-next = lambda x: x + dx
-print(accumulate(combiner, 0, 0, f, a + dx / 2, next, b, filter))
+print(accumulate(summ, 0, 0, f, a + dx / 2, next_value, b, filter))
